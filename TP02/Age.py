cac536208bdbdc91136d80fe6e18296a2719ccaf
Count-Pro68 <mira.imene.pro.68@gmail.com>
2024-11-08 17:04:01 +0100

    maj

--- TP02/Age.py
+++ TP02/Age.py
@@ -1,5 +1,4 @@
 age=int(input("Donnez votre âge : "))
 annee=int(2024-age)
 
-
 print("Votre annee de naissance est : ", annee)
--- TP02/exe1.py
+++ TP02/exe1.py
@@ -1,6 +1,5 @@
 x=int(input("Entrez x:"))
 y=int(input("Entrez y:"))
-
 print("Avant permutation :")
 print("x:", x)
 print("y:", y)
